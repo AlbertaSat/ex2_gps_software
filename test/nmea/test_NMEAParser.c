24d42fc402f0047dc7df5e5a5e4e92750b6876b7
Taylorr82 <taylorr82@macewan.ca>
2020-12-22 12:00:53 -0700

    added test to ensure the input function will correctly set the new data flag to true

--- test/nmea/test_NMEAParser.c
+++ test/nmea/test_NMEAParser.c
@@ -12,15 +12,23 @@ void tearDown(void)
 }
 
 
-void test_NMEAParser_decode_sentence_with_GPRMC(void) {
+
+
+void test_NMEAParser_will_take_input_sentence(void) {
+    // clear the values before the test to prevent interference
+    NMEAParser_reset_all_values();
     char *NMEA_to_decode = "$GPRMC,165401.997831,A,3725.0690,N,12203.3824,W,0.000,082.1,070717,,,A*48\r\n";
-    strcpy(_sentence, NMEA_to_decode);
-    NMEAParser_decode_sentence();
-    //TEST_ASSERT_EQUAL_INT32()
-}
+    for (int i = 0; i<strlen(NMEA_to_decode); i++) {
+        bool new_data = false;
 
-
-void test_NMEAParser_NeedToImplement(void)
-{
-    TEST_IGNORE_MESSAGE("Need to Implement NMEAParser");
+        // NMEAParser expects data to be given one char at a time
+        // until \n is reached
+        new_data = NMEAParser_encode(NMEA_to_decode[i]);
+        if (NMEA_to_decode[i] == '\n'){
+            TEST_ASSERT_EQUAL(true, new_data);
+        } else {
+            TEST_ASSERT_EQUAL(false, new_data);
+        }
+        
+    }
 }
