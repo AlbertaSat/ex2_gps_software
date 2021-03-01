dc9fcc807636c64a78f8a5b29be4a7eb3b578151
Taylorr82 <taylorr82@macewan.ca>
2021-02-28 18:28:05 -0700

    ceedling tests are soon to be deprecated

--- test/skyTraq/test_skyTraq_binary.c
+++ test/skyTraq/test_skyTraq_binary.c
@@ -10,7 +10,15 @@ void tearDown(void)
 {
 }
 
-void test_skyTraq_binary_NeedToImplement(void)
-{
-    TEST_IGNORE_MESSAGE("Need to Implement skyTraq_binary");
+void test_skytraq_checksum_will_pass(void) {
+    // configure nmea message interval example
+    uint8_t message_to_decode[] = {0xA0, 0xA1, 0x00, 0x09, 0x08, 0x01, 0x01, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x08, 0x0D, 0x0A};
+    bool result = skytraq_verify_checksum(message_to_decode, 9, 8);
+    TEST_ASSERT_EQUAL(true,result);
+}
+
+void test_skytraq_checksum_will_fail(void) {
+    uint8_t message_to_decode[] = {0xA0, 0xA1, 0x00, 0x09, 0x08, 0x01, 0x01, 0x01, 0x00, 0x01, 0xFF, 0x00, 0x00, 0x08, 0x0D, 0x0A};
+    bool result = skytraq_verify_checksum(message_to_decode, 9, 8);
+    TEST_ASSERT_EQUAL(false,result);
 }
