61b7de82158193a7d3e6833e83330848e117d808
Robert <taylorr82@mymacewan.ca>
2021-03-08 16:44:12 -0700

    refactor to match CDR

---
+++ ceedling-deprecated/inc/drivers/sci_ceed.h
@@ -0,0 +1,17 @@
+
+#include <stdint.h>
+#define uint32 uint32_t
+#define uint8 uint8_t
+/** @def scilinREG
+*   @brief SCILIN (LIN - Compatibility Mode)  Register Frame Pointer
+*
+*   This pointer is used by the SCI driver to access the sci module registers.
+*/
+#define scilinREG ((sciBASE_t *)0xFFF7E400U)
+
+typedef volatile struct sciBase
+{
+    uint32 hello;
+} sciBASE_t;
+// function definiton copied from HalCoGen
+void sciSend(sciBASE_t *sci, uint32 length, uint8 * data);
